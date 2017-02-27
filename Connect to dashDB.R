42b4de6b3d60c3d2590c7e4d9913c452bc11ea11
CatherineCao2016 <caoying0824@gmail.com>
2017-02-27 11:07:44 -0500

    Update Connect to dashDB.R

--- Connect to dashDB.R
+++ Connect to dashDB.R
@@ -16,28 +16,6 @@ conn_path <- paste(dsn_driver,
                    ";UID=",dsn_uid,
                    ";PWD=",dsn_pwd,sep="")
 
-ch <- idaConnect(conn_path)  
-idaInit(ch)  
-
-library(ibmdbR)
-
-# connect to project dashDB
-
-dsn_driver <- "BLUDB" 
-dsn_database <- "BLUDB"  
-dsn_hostname <-  "dashdb-entry-yp-dal09-10.services.dal.bluemix.net"  
-dsn_port <- "50000"  
-dsn_protocol <- "TCPIP"  
-dsn_uid <- "dash13990"
-dsn_pwd <- "a8ae96f334fb"
-
-conn_path <- paste(dsn_driver,  
-                   ";DATABASE=",dsn_database,
-                   ";HOSTNAME=",dsn_hostname,
-                   ";PORT=",dsn_port,
-                   ";PROTOCOL=",dsn_protocol,
-                   ";UID=",dsn_uid,
-                   ";PWD=",dsn_pwd,sep="")
 
 con <- idaConnect(conn_path)  
 idaInit(con)  
